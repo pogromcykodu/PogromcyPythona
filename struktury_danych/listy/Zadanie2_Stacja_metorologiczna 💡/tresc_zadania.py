93736a0a79813fcfe885ea608642f55f3cc3848d
Julita <julita@rootxnet.com>
2021-02-02 23:37:20 +0100

    List exercises - Zadanie2_Stacja_meteorologiczna

---
+++ struktury_danych/listy/Zadanie2_Stacja_metorologiczna 💡/tresc_zadania.py
@@ -0,0 +1,13 @@
+"""
+Pracujesz w stacji meteorologicznej i dostałeś właśnie nowe zadanie.
+Przed Tobą lista wahań temperatury z ostatniego tygodnia:
+
+1.5, 3, 2, 0, -1 , 1.9, 0.1
+
+Twoim zadaniem jest podanie informacji o:
+a) najwyższej zanotowanej temperaturze
+b) najniższej zanotowanej temperaturze
+c) średnią temperatur
+d) posortowaną listę temperatur, od najmniejszej do największej
+e) posortowaną listę temperatur, od największej do najmniejszej
+"""
