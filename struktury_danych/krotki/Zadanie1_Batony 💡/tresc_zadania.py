553fdbf496a1bcd166e78159553011ca5e7476a9
Julita <julita@rootxnet.com>
2021-02-03 11:30:17 +0100

    Tuples - Zadanie1_Batony & Lists - Zadanie1_Batony

---
+++ struktury_danych/krotki/Zadanie1_Batony 💡/tresc_zadania.py
@@ -0,0 +1,16 @@
+"""
+Jesteś ankieterem, Twoim zadaniem jest zebrać informację o ulubionych batonikach.
+Każdy ankietowany podaje swój ulubiony batonik.
+
+Zebrane dane:
+"Snikers", "Bounty", "Mars", "WW", "Bounty", "Duplo", "Prince Polo", "Twix", "Snikers", "WW",  "Duplo", "Bounty"
+
+a) Stwórz krotkę batoników z zebranych danych
+b) Wyświetl całą krotkę
+c) Wyświetl pierwszy i ostatni element krotki
+d) Policz ile osób wybrało "Bounty" jako swój ulubiony batonik
+e) Sprawdź czy baton "BA" występuje w krotce
+i) Stwórz nową krotkę zawierająca ulubione ciastka
+j) Stwórz nową krotkę 'sweets', która będzie zawierać wszystkie batony i ciastka (połącz obie krotki)
+
+"""
---
+++ struktury_danych/listy/Zadanie1_Batony 💡/tresc_zadania.py
@@ -0,0 +1,18 @@
+"""
+Jesteś ankieterem, Twoim zadaniem jest zebrać informację o ulubionych batonikach.
+Każdy ankietowany podaje swój ulubiony baton.
+
+Zebrane dane:
+"Snikers", "Bounty", "Mars", "WW", "Bounty", "Duplo", "Prince Polo", "Twix", "Snikers", "WW",  "Duplo", "Bounty"
+
+a) Stwórz listę batoników z zebranych danych
+b) Do listy dodaj swój ulubiony baton (użyj odpowiedniej funkcji, aby dodać element na listę)
+c) Wyświetl całą listę
+d) Wyświetl pierwszy i ostatni element listy
+e) Policz ile osób wybrało "Bounty" jako swój ulubiony baton
+f) Sprawdź czy baton "BA" występuje na liście
+g) Usuń z listy pierwszy element (użyj odpowiedniej funkcji, aby dodać element na listę)
+h) Stwórz nową listę zawierająca ulubione ciastka
+i) Stwórz nową listę 'sweets', która będzie zawierać wszystkie batony i ciastka (połącz obie listy)
+
+"""
